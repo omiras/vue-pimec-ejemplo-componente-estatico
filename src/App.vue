dfc5a05133136c419ac7f8ecc1d3163b8802ee93
omiras <oscar.miras@gmail.com>
2023-11-14 12:36:08 +0100

    updated

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,14 @@
 import Animal from "./components/Animal.vue"
 import Counter from "./components/Counter.vue"
 
+const vaccinesArray = [{
+  name: "Rabia",
+  date: "2021-01-01"
+}, {
+  name: "Moquillo",
+  date: "2021-02-15"
+}];
+
 </script>
 
 <!-- tag template donde va todo nuestro HTML -->
@@ -15,9 +23,11 @@ import Counter from "./components/Counter.vue"
     <h1>Protectora de animales</h1>
     <p>Bienvenido a Progats, tu protectora de proximidad</p>
     <section class="animal-list">
-      <Animal />
-      <Animal />
-      <Animal />
+      <Animal name="Bigotes" :weight="3" image="https://cdn2.thecatapi.com/images/6v3.jpg" birth-date="2020-04-04"
+        :vaccines="vaccinesArray" />
+      <Animal name="Loki" :weight="5" image="https://cdn2.thecatapi.com/images/67t.jpg" birth-date="2019-09-07"
+        :vaccines="[]" />
+
     </section>
     <section>
       <h2>Perros adoptados hasta el momento</h2>
@@ -33,6 +43,7 @@ import Counter from "./components/Counter.vue"
 <style>
 .animal-list {
   display: flex;
+  gap: 1rem;
 }
 
 .header {
--- src/components/Animal.vue
+++ src/components/Animal.vue
@@ -1,7 +1,35 @@
+<script setup>
+/** Es un array de strings con los atributos arbitrarios que hemos definido en <Animal /> 
+ * Vamos a tener a nuestra disposición una variable para usar en el JS o el template
+ */
+const props = defineProps(['name', 'image', 'alt', 'birthDate', 'vaccines', 'weight'])
+
+// peso ideal
+const idealWeight = 5;
+
+const clicCat = () => {
+    console.log(`My name is ${props.name}`)
+}
+
+</script>
+
 <template>
-    <article>
-        <h2>Bigotes</h2>
-        <img src="https://cdn2.thecatapi.com/images/6v3.jpg" alt="">
+    <article @click="clicCat">
+        <h2>{{ name }}</h2>
+        <h3>Peso {{ weight }} kg </h3>
+        <h3>Fecha nacimiento {{ birthDate }}</h3>
+        <h3>La diferencia de peso entre el animal y el peso ideal es {{ Math.abs(weight - idealWeight) }}. Esto podria ser
+            una computed property, pues es una información que se puede calcular en función de otras variables </h3>
+        <img :src="image" :alt="name">
+        <section>
+            <p v-if="vaccines.length == 0">¡El gato no tiene vacunas! Está en peligro...</p>
+            <p v-else>
+            <ul>
+                <li v-for="v in vaccines">La vacuna {{ v.name }} fue administrada en la fecha {{ v.date }}</li>
+            </ul>
+
+            </p>
+        </section>
     </article>
 </template>
 
